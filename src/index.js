ef1a8275f48f04ad91800108c8770ccffd3620c7
Kay Watanabe <kay.watanabe80@gmail.com>
2020-02-08 15:41:27 -0800

    Setup Sass files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './styles/styles.scss';
+import './styles/styles.scss';
+import './styles/fonts/grbook.ttf';
 import AppRouter from './routers/AppRouter';
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
+
+
