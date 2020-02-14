8966e7b0e1dfab532fec001b1a8369f1a7d69f19
Kay Watanabe <kay.watanabe80@gmail.com>
2020-02-14 02:32:26 -0800

    fixed dropdown filter

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,61 +1,35 @@
-import React, { useState }  from 'react';
+import React from 'react';
 
 
+const charts = [ 'popular', 'top_rated', 'now_playing', 'upcoming'];
 
-
+const makeChartOptions = (charts) => {
+    return charts.map((chartGroup, i) => {  
+        return (
+            <option key={i} value={chartGroup}>{chartGroup}</option>
+            )});
+}
 
 
 const Chart = (props) => {
     // this handleChangeChart is not the same as in Home.js
     // this is local
-
-    const [ selectActive, setSelectState ] = useState(false);
-    const [rankSelection, setRankSelection] = useState(props.chart);
-
-    const charts = [ 'popular', 'top_rated', 'now_playing', 'upcoming'];
-
-    
-
-    const makeChartOptions = (charts) => {
-        
-        return charts.map((chartGroup, i) => {
-            
-            return (
-                
-                <span key={i} 
-                    value={chartGroup} 
-                    
-                    className={`custom-option ${chartGroup === rankSelection ? 'selected' : ''} `}
-                    onClick={() => { setRankSelection(chartGroup)}}
-                    >
-                        {chartGroup}
-                </span>
-            
-            )
-        
-        }
-        );
+    const handleChangeChart = (e) => {
+        e.preventDefault();
+        const loc = e.target.value;
+        props.handleChangeChart(loc);
     }
-   
- 
-    
+
     return (
-        <div className="custom-select-wrapper">
-             <div className="sort-by">
-                <span className="sort-by-text">Show me</span>
-                <div className={`custom-select ${selectActive ? "open" : ""}`}>
-                    <div className="custom-select__trigger">
-                        <span>{rankSelection}</span>
-                        
-                        <div className="arrow" onClick={()=>setSelectState((prevSelectActive)=> !prevSelectActive)}></div>
-                    </div>{/** end of custom-select__trigger */}
-                    <div className="custom-options">
-                        {makeChartOptions(charts, rankSelection)}
-    
-                    </div>{/** end of custom-options */}
-                </div>{/** end of custom-select */}
-            </div> {/** end of sort-by */}
-        </div>//end of custom-select-wrapper
+     <div className="charts">
+            {/* <h3>{props.chart}</h3> */}
+            <form>
+                <label htmlFor="selectchart">Show me</label>
+                <select name="selectchart" id="selectchart" onChange={handleChangeChart}>
+                    {makeChartOptions(charts, props.chart)}  
+                </select>
+            </form>
+        </div>
     );
 
 };
--- src/components/Year.js
+++ src/components/Year.js
@@ -1,69 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const years = [ 2022,  2021,  2020, 2019, 2018, 2017, 2016,2015];
  
-
+const makeYearOptions = (years) => {
+    return years.map((yearGroup, i) => {  
+        return (
+            <option key={i} value={yearGroup}>{yearGroup}</option> 
+            )});
+}
 
 const Year = (props) => {
     // this handleChangeYear is not the same as in Home.js
     // this is local
-    const [selectYearActive, setSelectYear ] = useState(false);
-    const [yearSelection, setYearSelection] = useState(props.year);
-
-    const YearBoxToggleClass = (e)=>{
-        setSelectYear(!selectYearActive);
-    }
-
    
-    const YearSelectedToggleClass = (yearGroup)=>{
-        setYearSelection(yearGroup);
-    //setSelectYear(!selectYearActive);
-        
+    const handleChangeYear = (e) => {
+        e.preventDefault();
+        const loc = e.target.value;
+        props.handleChangeYear(loc);
     }
 
-   
-    
-    const makeYearOptions = (years) => {
-       
-        return years.map((yearGroup, i) => {  
-            return (
-                <span key={i} 
-                      value={yearGroup}
-                      className={`custom-option ${yearGroup === yearSelection ?'selected' : ''}`}
-                      //onClick ={()=>setYearSelection(yearGroup)}
-                     onClick ={YearSelectedToggleClass}
-                     //onChange = {handleChange}
-                    >
-                          
-                     {yearGroup}    
-                </span>
-
-               
-               
-                );
-            });
-    }
-
-  
 
     return (
-        <div className="custom-select-wrapper">
-            <div className="sort-by choose-year">
-                <span className="choose-year-text">movie released in</span>
-                <div className={`custom-select ${selectYearActive ? "open" : ""}`}>
-                    <div className="custom-select__trigger">
-                        <span>{yearSelection}</span>
-                        <div className="arrow" 
-                             onClick={YearBoxToggleClass}>
-                                
-                        </div>
-                    </div>{/* end of custom-select__trigger */}
-                    <div className="custom-options">
-                    {makeYearOptions(years, yearSelection)} 
-                    </div>{/* end of ccustom-options */}
-                </div>{/* end of custom-select */}
-            </div> {/* end of sort-by choose-year */}
-        </div> // end of custom-select-wrapper
+        <div className="year">
+        {/* <h3>{props.year}</h3> */}
+        <form>
+            <label htmlFor="selectyear">movie released in</label>
+            <select name="selectyear" id="selectyear" onChange={handleChangeYear}>
+                {makeYearOptions(years, props.year)}  
+            </select>
+            
+        </form>
+    </div>
     );
 
 };
