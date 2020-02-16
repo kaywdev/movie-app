368e1a2872b8694447dd80ab0016630c324d5137
Zahra Fatemi <fatemi01@gmail.com>
2020-02-15 19:01:47 -0800

    default poster added

--- src/utilities/movieMaker.js
+++ src/utilities/movieMaker.js
@@ -211,8 +211,8 @@ function setImage(obj) {
         obj.bgimg = iconPath1280 + obj.bgimg;
         obj.poster = obj.bgimg;
     }else if(obj.bgimg === null && obj.poster === null){
-        obj.poster = rateIconPath + 'zero-grey.png';
-        obj.bgimg = rateIconPath + 'zero-grey.png';
+        obj.poster = rateIconPath + 'defaultPoster.png';
+        obj.bgimg = rateIconPath + 'defaultPoster.png';
     }else{
         obj.poster = iconPath + obj.poster;
         obj.bgimg = iconPath1280 + obj.bgimg;
