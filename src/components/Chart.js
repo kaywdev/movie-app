efc49da8f0d3aea6e81236a5feb85becc268285c
Zahra Fatemi <fatemi01@gmail.com>
2020-02-16 04:04:12 -0800

    year default to 2020

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,19 +1,15 @@
 import React from 'react';
 
-
 const charts = [ 'popular', 'top_rated', 'now_playing', 'upcoming'];
 
 const makeChartOptions = (charts) => {
     return charts.map((chartGroup, i) => {  
         return (
-            <option key={i} value={chartGroup}>{chartGroup}</option>
+            <option key={i} value={chartGroup} >{chartGroup}</option>
             )});
 }
 
-
 const Chart = (props) => {
-    // this handleChangeChart is not the same as in Home.js
-    // this is local
     const handleChangeChart = (e) => {
         e.preventDefault();
         const loc = e.target.value;
@@ -21,8 +17,7 @@ const Chart = (props) => {
     }
 
     return (
-     <div className="charts">
-            {/* <h3>{props.chart}</h3> */}
+        <div className="charts">
             <form>
                 <label htmlFor="selectchart">Show me</label>
                 <select name="selectchart" 
