abf12c1dcca9aa2694e5c8de18204ec6bc97868d
Zahra Fatemi <fatemi01@gmail.com>
2020-02-16 02:47:47 -0800

    click outside search so the results disappear

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 import movieMaker from '../utilities/movieMaker';
 import Search from './Search';
@@ -8,6 +8,27 @@ const SearchBar = () => {
 
     const key = "65a9ed7abe7e75b3c0bf9250934f2b49";
     const search_api = "https://api.themoviedb.org/3/search/movie?api_key=";
+    let outClick =false;
+
+    const node = useRef();
+    useEffect(() => {
+        // add when mounted
+        document.addEventListener("mousedown", handleClick);
+        // return function to be called when unmounted
+        return () => {
+          document.removeEventListener("mousedown", handleClick);
+        };
+      }, []);
+
+
+      const handleClick = e => {
+        if (node.current.contains(e.target)) {
+          // inside click
+          return;
+        }
+        // outside click 
+        setResult([]);
+    };
 
     const [result, setResult] = useState([]);
 
@@ -15,16 +36,16 @@ const SearchBar = () => {
     const handleSearch = (searchEnteredByUser) => {
         const s = searchEnteredByUser;
         if(s !== ''){
-        axios(search_api + key + "&query=" + s).then(({ data }) => {
-            setResult(movieMaker(data.results));
-        });
-    }else{
-        setResult([]);
-    }
+            axios(search_api + key + "&query=" + s).then(({ data }) => {
+                setResult(movieMaker(data.results));
+            });
+        }else{
+            setResult([]);
+        }
     }
 
     return (
-        <div>
+        <div ref={node}>
             <Search handleSearch={handleSearch} search={null} />
             <div className="movielist-wrapper">
                 <Results results={result} />
