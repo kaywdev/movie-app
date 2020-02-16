84555a951d0aafb872b85a4883e0f3958ac38d1b
Zahra Fatemi <fatemi01@gmail.com>
2020-02-16 02:17:59 -0800

    added this so if delete what typed on search so results will disapear

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import searchIcon from '../images/search.svg';
+import Home from './Home';
 
 const Search = ({ handleSearch }) => {
 
     const handleFormSubmit = (e) => {
-        e.preventDefault();
-        handleSearch(e.target.value);
-    }
-    const handleFormKeySubmit = (e) => {
-        if (e.key === 'Enter') {
             e.preventDefault();
             handleSearch(e.target.value);
-        }
+            //console.log('in change: '+e.target.value);
+            //console.log(e.target.value.length);
+    }
+    const handleFormKeySubmit = (e) => {
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                handleSearch(e.target.value);
+                //console.log('in enter: '+e.target.value);
+            }
     }
     
     return (
         <section className="searchbox-wrap"> 
-            <span className="search-icon"><img src={searchIcon} alt="search-icon"/></span>
+            <span className="search-icon">
+                <img src={searchIcon} alt="search-icon"/></span>
             <form>
                 {/* label sholud be hidden from HTML by CSS */}
                 <label htmlFor="search" className="sr-only">Search: </label> 
